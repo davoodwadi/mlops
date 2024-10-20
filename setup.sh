6254b2de732afd353db46fc8a6d898d03ff333ce
Davood Wadi <62418145+davoodwadi@users.noreply.github.com>
2024-10-20 13:58:57 +0330

    renamed workspace name to pythonfoundations

--- setup.sh
+++ setup.sh
@@ -27,10 +27,10 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-#RESOURCE_GROUP_NAME="cloudml"
+# RESOURCE_GROUP_NAME="default_resource_group"
 GROUP="default_resource_group"
 LOCATION="eastus"
-WORKSPACE="test_workspace_azure_ml"
+WORKSPACE="pythnfoundations29sep"
 # </set_variables>
 
 
